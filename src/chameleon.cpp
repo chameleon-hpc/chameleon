91adb9ec5f6a38701816c80732ce8a241143fdf4
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2018-06-21 18:23:53 +0200

    added calls to libffi for testing and implemented target task queue

--- src/chameleon.cpp
+++ src/chameleon.cpp
@@ -1,19 +1,21 @@
 #include "chameleon.h"
+#include <ffi.h>
+#include <list>
+#include <mpi.h>
+#include <mutex>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <mpi.h>
+#include <vector>
 
-#include <list>
-// #include <thread>         // std::thread
-#include <mutex>          // std::mutex
+static std::mutex _mtx_data_entry;
+static std::list<OffloadingDataEntryTy> _data_entries;
+static std::mutex _mtx_tasks;
+static std::list<OffloadingTaskEntryTy> _tasks;
 
-std::mutex _mtx_data_entry;
-std::list<OffloadingDataEntryTy> _data_entries;
-
-MPI_Comm chameleon_comm;
-int chameleon_comm_rank;
-int chameleon_comm_size;
+static MPI_Comm chameleon_comm;
+static int chameleon_comm_rank;
+static int chameleon_comm_size;
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,22 +38,84 @@ int32_t chameleon_init() {
     printf("Chameleon: Hello from rank %d of %d\n", chameleon_comm_rank, chameleon_comm_size);
 
     // dummy target region to force binary loading
+    // currently the 
     #pragma omp target device(1001) // 1001 = CHAMELEON_HOST
     {
     }
 
     // TODO: create communication thread (maybe start later)
 
-    return 0;
+    return CHAM_SUCCESS;
 }
 
 int32_t chameleon_finalize() {
-    return 0;
+    return CHAM_SUCCESS;
 }
 
 int32_t chameleon_distributed_taskwait() {
     // TODO: remember to remove entries from _data_entries as soon as reference count hits 0
-    return 0;
+
+    // first shot is to execute the tasks in place
+    while(true) {
+        // DEBUG info
+        bool test1 = _tasks.empty();
+        size_t test2 = _tasks.size();
+
+        if(_tasks.empty())
+            break;
+        
+        // get first task
+        OffloadingTaskEntryTy cur_task = _tasks.front();
+        _tasks.pop_front();
+
+        // Use libffi to launch execution.
+        ffi_cif cif;
+
+        // All args are references.
+        std::vector<ffi_type *> args_types(cur_task.arg_num, &ffi_type_pointer);
+        std::vector<void *> args(cur_task.arg_num);
+        std::vector<void *> ptrs(cur_task.arg_num);
+
+        for (int32_t i = 0; i < cur_task.arg_num; ++i) {
+            ptrs[i] = (void *)((intptr_t)cur_task.tgt_args[i] + cur_task.tgt_offsets[i]);
+            args[i] = &ptrs[i];
+
+            // TODO: here we need to perform a pointer mapping to source pointers 
+            // because target pointers have already been deleted
+            if(cur_task.tgt_arg_types[i] && CH_OMP_TGT_MAPTYPE_LITERAL) {
+                // no need to do anything because it is by value
+                continue;
+            }
+
+            // get source pointer for tgt_ptr
+            int found = 0;
+            for(auto &entry : _data_entries) {
+                if(entry.tgt_ptr == args[i]) {
+                    // increase reference count
+                    args[i] = entry.hst_ptr;
+                    found = 1;
+                    break;
+                }
+            }
+            if(!found) {
+                // something went wrong here
+            }
+        }
+
+        ffi_status status = ffi_prep_cif(&cif, FFI_DEFAULT_ABI, cur_task.arg_num,
+                                        &ffi_type_void, &args_types[0]);
+
+        if(status != FFI_OK) {
+            //assert(status == FFI_OK && "Unable to prepare target launch!");
+            return CHAM_FAILURE;
+        }
+
+        void (*entry)(void);
+        *((void**) &entry) = cur_task.tgt_entry_ptr;
+        ffi_call(&cif, entry, NULL, &args[0]);
+    }
+
+    return CHAM_SUCCESS;
 }
 
 int32_t chameleon_submit_data(void *tgt_ptr, void *hst_ptr, int64_t size) {
@@ -72,37 +136,48 @@ int32_t chameleon_submit_data(void *tgt_ptr, void *hst_ptr, int64_t size) {
         _data_entries.push_back(new_entry);
     }
     _mtx_data_entry.unlock();
-    return 0;
+    return CHAM_SUCCESS;
 }
 
-int32_t chameleon_submit_implicit_data(void *tgt_ptr) {
-    // check whether matching mapped entry already in lookup table
-    _mtx_data_entry.lock();
-    for(auto &entry : _data_entries) {
-        if(entry.tgt_ptr == tgt_ptr && entry.is_implicit == 0) {
-            // there is already an entry that has explicitly been mapped
-            return 0;
-        }
-    }
+int32_t chameleon_add_task(OffloadingTaskEntryTy task) {
+    _mtx_tasks.lock();
+    _tasks.push_back(task);
 
-    // copy value that is represented by pointer and insert in table + mark as implicit (can be deleted after work package)
-    // Q: we currently assume that each agrument has 8 Byte.. Only addresses, some of which represent an address and some an actual value
-    // Calling interface is not providing information about size of arguments
-
-    // pointer value is not really a pointer but representing a numeric value
-    double tt = *(double*)&tgt_ptr;
-    double * pp = (double *) malloc(sizeof(double));
-    *pp = tt;
-    void * vpp = (void *)pp;
-
-    // new entry + mark as implicit
-    // OffloadingDataEntryTy new_entry(tgt_ptr, vpp, 8);
-    // new_entry.is_implicit = 1;
-    // add to list
-    // _data_entries.push_back(new_entry);
-    _mtx_data_entry.unlock();
-    return 0;
+    bool test1 = _tasks.empty();
+    size_t test2 = _tasks.size();
+    _mtx_tasks.unlock();
+    return CHAM_SUCCESS;
 }
+
+// int32_t chameleon_submit_implicit_data(void *tgt_ptr) {
+//     // check whether matching mapped entry already in lookup table
+//     _mtx_data_entry.lock();
+//     for(auto &entry : _data_entries) {
+//         if(entry.tgt_ptr == tgt_ptr && entry.is_implicit == 0) {
+//             // there is already an entry that has explicitly been mapped
+//             _mtx_data_entry.unlock();
+//             return 0;
+//         }
+//     }
+
+//     // copy value that is represented by pointer and insert in table + mark as implicit (can be deleted after work package)
+//     // Q: we currently assume that each agrument has 8 Byte.. Only addresses, some of which represent an address and some an actual value
+//     // Calling interface is not providing information about size of arguments
+
+//     // pointer value is not really a pointer but representing a numeric value
+//     double tt = *(double*)&tgt_ptr;
+//     double * pp = (double *) malloc(sizeof(double));
+//     *pp = tt;
+//     void * vpp = (void *)pp;
+
+//     // new entry + mark as implicit
+//     // OffloadingDataEntryTy new_entry(tgt_ptr, vpp, 8);
+//     // new_entry.is_implicit = 1;
+//     // add to list
+//     // _data_entries.push_back(new_entry);
+//     _mtx_data_entry.unlock();
+//     return CHAM_SUCCESS;
+// }
 
 #ifdef __cplusplus
 }
--- src/chameleon.h
+++ src/chameleon.h
@@ -5,6 +5,85 @@
 #include <cstdint>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+// TODO: fix that to have only one place where that is defined
+// copy of OpenMP target types
+enum chameleon_tgt_map_type {
+  // No flags
+  CH_OMP_TGT_MAPTYPE_NONE            = 0x000,
+  // copy data from host to device
+  CH_OMP_TGT_MAPTYPE_TO              = 0x001,
+  // copy data from device to host
+  CH_OMP_TGT_MAPTYPE_FROM            = 0x002,
+  // copy regardless of the reference count
+  CH_OMP_TGT_MAPTYPE_ALWAYS          = 0x004,
+  // force unmapping of data
+  CH_OMP_TGT_MAPTYPE_DELETE          = 0x008,
+  // map the pointer as well as the pointee
+  CH_OMP_TGT_MAPTYPE_PTR_AND_OBJ     = 0x010,
+  // pass device base address to kernel
+  CH_OMP_TGT_MAPTYPE_TARGET_PARAM    = 0x020,
+  // return base device address of mapped data
+  CH_OMP_TGT_MAPTYPE_RETURN_PARAM    = 0x040,
+  // private variable - not mapped
+  CH_OMP_TGT_MAPTYPE_PRIVATE         = 0x080,
+  // copy by value - not mapped
+  CH_OMP_TGT_MAPTYPE_LITERAL         = 0x100,
+  // mapping is implicit
+  CH_OMP_TGT_MAPTYPE_IMPLICIT        = 0x200,
+  // member of struct, member given by 16 MSBs - 1
+  CH_OMP_TGT_MAPTYPE_MEMBER_OF       = 0xffff000000000000
+};
+
+enum chameleon_result_types {
+    CHAM_SUCCESS = 0,
+    CHAM_FAILURE = 1
+};
+
+struct OffloadingTaskEntryTy {
+    void *tgt_entry_ptr;
+    void **tgt_args;
+    ptrdiff_t *tgt_offsets;
+    int64_t *tgt_arg_types;
+    int32_t arg_num;
+
+    OffloadingTaskEntryTy() {
+        tgt_entry_ptr = nullptr;
+        tgt_args = nullptr;
+        tgt_offsets = nullptr;
+        tgt_arg_types = nullptr;
+        arg_num = 0;
+    }
+
+    OffloadingTaskEntryTy(
+        void *p_tgt_entry_ptr, 
+        void **p_tgt_args, 
+        ptrdiff_t *p_tgt_offsets, 
+        int64_t *p_tgt_arg_types, 
+        int32_t p_arg_num) {
+            arg_num = p_arg_num;
+            tgt_entry_ptr = p_tgt_entry_ptr;
+
+            tgt_args = (void**) malloc(sizeof(void*) * p_arg_num);
+            tgt_offsets = (ptrdiff_t *) malloc(sizeof(ptrdiff_t) * p_arg_num);
+            tgt_arg_types = (int64_t *) malloc(sizeof(int64_t) * p_arg_num);
+
+            for(int i = 0; i < p_arg_num; i++){
+                tgt_args[i] = p_tgt_args[i];
+                tgt_offsets[i] = p_tgt_offsets[i];
+                tgt_arg_types[i] = p_tgt_arg_types[i];
+            }
+            //tgt_args = p_tgt_args;
+            //tgt_offsets = p_tgt_offsets;
+            //tgt_arg_types = p_tgt_arg_types;
+        }
+    // ~OffloadingTaskEntryTy() {
+    //     free(tgt_args);
+    //     free(tgt_offsets);
+    //     free(tgt_arg_types);
+    // }
+};
 
 struct OffloadingDataEntryTy {
     void *tgt_ptr;
@@ -13,17 +92,14 @@ struct OffloadingDataEntryTy {
     int32_t ref_count;
 
     // does it make sense to put these two here? (might be used in multiple target regions)
-    int32_t send_back;
-    int32_t is_implicit;
+    // int32_t send_back;
+    // int32_t is_implicit;
 
     OffloadingDataEntryTy() {
         tgt_ptr = nullptr;
         hst_ptr = nullptr;
         size = 0;
         ref_count = 1;
-
-        send_back = 0;
-        is_implicit = 0;
     }
 
     OffloadingDataEntryTy(void *p_tgt_ptr, void *p_hst_ptr, int64_t p_size) {
@@ -31,9 +107,6 @@ struct OffloadingDataEntryTy {
         hst_ptr = p_hst_ptr;
         size = p_size;
         ref_count = 1;
-
-        send_back = 0;
-        is_implicit = 0;
     }
 };
 
@@ -49,7 +122,7 @@ int32_t chameleon_distributed_taskwait();
 
 int32_t chameleon_submit_data(void *tgt_ptr, void *hst_ptr, int64_t size);
 
-int32_t chameleon_submit_implicit_data(void *tgt_ptr);
+int32_t chameleon_add_task(OffloadingTaskEntryTy task);
 
 #ifdef __cplusplus
 }

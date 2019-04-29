eac5e17f5d9df22332b3bdf4a9d7b7d257d536b6
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2019-04-29 16:19:45 +0200

    added flag for multithreading (some older versions might need that) + minor optimization + added additional statistics for timings in comm thread

--- src/cham_statistics.cpp
+++ src/cham_statistics.cpp
@@ -41,6 +41,12 @@ std::atomic<int>     _time_encode_count(0);
 std::atomic<double>  _time_decode_sum(0.0);
 std::atomic<int>     _time_decode_count(0);
 
+std::atomic<double>  _time_between_load_exchange_sum(0.0);
+std::atomic<int64_t> _time_between_load_exchange_count(0);
+
+std::atomic<double>  _time_between_allgather_and_exchange_sum(0.0);
+std::atomic<int64_t> _time_between_allgather_and_exchange_count(0);
+
 #if CHAMELEON_TOOL_SUPPORT
 std::atomic<double>  _time_tool_get_thread_data_sum(0.0);
 std::atomic<int>     _time_tool_get_thread_data_count(0);
@@ -86,6 +92,12 @@ void cham_stats_reset_for_sync_cycle() {
 
     _time_decode_sum = 0.0;
     _time_decode_count = 0;
+
+    _time_between_load_exchange_sum = 0.0;
+    _time_between_load_exchange_count = 0;
+
+    _time_between_allgather_and_exchange_sum = 0.0;
+    _time_between_allgather_and_exchange_count = 0;
 }
 
 void cham_stats_print_stats_w_mean(std::string name, double sum, int count, bool cummulative = false) {
@@ -123,6 +135,8 @@ void cham_stats_print_stats() {
     cham_stats_print_stats_w_mean("_time_comm_back_recv_sum", _time_comm_back_recv_sum, _time_comm_back_recv_count);
     cham_stats_print_stats_w_mean("_time_encode_sum", _time_encode_sum, _time_encode_count);
     cham_stats_print_stats_w_mean("_time_decode_sum", _time_decode_sum, _time_decode_count);
+    cham_stats_print_stats_w_mean("_time_between_load_exchange_sum", _time_between_load_exchange_sum, _time_between_load_exchange_count);
+    cham_stats_print_stats_w_mean("_time_between_allgather_and_exchange_sum", _time_between_allgather_and_exchange_sum, _time_between_allgather_and_exchange_count);
 
     cham_stats_print_stats_w_mean("_time_data_submit_sum", _time_data_submit_sum, _time_data_submit_count, true);
 #if CHAMELEON_TOOL_SUPPORT

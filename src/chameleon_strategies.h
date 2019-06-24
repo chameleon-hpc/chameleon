85be9bbcc889a986b32db0ae017cde6d6d4be841
Philipp Samfass <philipp.samfass@tum.de>
2019-06-24 15:54:21 +0200

    do not add stolen tasks to replication queue + count load information correctly

--- src/chameleon_strategies.h
+++ src/chameleon_strategies.h
@@ -11,6 +11,6 @@ void compute_num_tasks_to_offload( std::vector<int32_t>& tasks_to_offload_per_ra
 
 void compute_num_tasks_to_replicate( std::vector<cham_replication_info_t>& replication_infos, std::vector<int32_t>& loadInfoRanks, int32_t num_tasks_local);
 
-extern int32_t getDefaultLoadInformationForRank(TYPE_TASK_ID* local_task_ids, int32_t num_tasks_local, TYPE_TASK_ID* stolen_task_ids, int32_t num_tasks_stolen);
+extern int32_t get_default_load_information_for_rank(TYPE_TASK_ID* local_task_ids, int32_t num_tasks_local, TYPE_TASK_ID* local_rep_task_ids, int32_t num_tasks_local_rep, TYPE_TASK_ID* stolen_task_ids, int32_t num_tasks_stolen, TYPE_TASK_ID* stolen_task_ids_rep, int32_t num_tasks_stolen_rep);
 
 #endif

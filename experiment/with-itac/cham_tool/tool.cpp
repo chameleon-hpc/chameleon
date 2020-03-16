844982d4fa9199dd472a967309f5fc4b72e03ab7
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-03-16 22:37:39 +0100

    update tool

--- experiment/with-itac/cham_tool/tool.cpp
+++ experiment/with-itac/cham_tool/tool.cpp
@@ -184,8 +184,8 @@ on_cham_t_callback_change_freq_for_execution(
     int rank = cham_t_get_rank_info()->comm_rank;
     if (load_info_per_rank <= half_processed_per_rank){
         printf("R%d, tasks_id=%d, cur_load = %d / total_created_tasks = %d\n", rank, chameleon_get_task_id(task), load_info_per_rank, total_created_tasks_per_rank);
-        // int32_t noise_time = 235050;    // make noise 50% slower for a mxm-task with size = 1024
-        int32_t noise_time = 507850;    // make noise 50% slower for non-uni tasks with size_range = {128, 256, 512, 1024, 2048}
+        noise_time = 235050;    // make noise 50% slower for a mxm-task with size = 1024
+        // noise_time = 507850;    // make noise 50% slower for non-uni tasks with size_range = {128, 256, 512, 1024, 2048}
     }
 
     return noise_time;

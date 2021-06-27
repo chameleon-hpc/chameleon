51f38e6f8a8576caefb6980d45425aaa7bbc705b
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2021-06-27 11:51:44 +0200

    implemented constructor version that runs before chameleon_init

--- src/chameleon.cpp
+++ src/chameleon.cpp
@@ -300,6 +300,18 @@ inline void verify_initialized() {
 }
 
 /* 
+ * Function chameleon_preinit
+ * Runs before chameleon library is initialized.
+ * NOTE: Might not work with libgomp: might not run before libgomp constructor 
+ *       is executed which already binds threads if OpenMP affinity policy set.
+ */
+static void __attribute__((constructor (102))) chameleon_preinit_ctor(void) {
+    // fprintf (stderr,"==> chameleon_preinit_ctor\n");
+    // first thing: remember original full cpuset of complete process
+    sched_getaffinity(getpid(), sizeof(cpu_set_t), &pid_mask);
+}
+
+/* 
  * Function chameleon_init
  * Initialized chameleon library, communicators and all whats necessary.
  */
@@ -314,8 +326,6 @@ int32_t chameleon_init() {
         return CHAM_SUCCESS;
     }
 
-    // first thing: remember original full cpuset of complete process
-    sched_getaffinity(getpid(), sizeof(cpu_set_t), &pid_mask);
     // === DEBUG
     // get_and_print_affinity_mask();
     // === DEBUG
@@ -350,16 +360,18 @@ int32_t chameleon_init() {
     MPI_Errhandler_set(chameleon_comm_cancel, MPI_ERRORS_RETURN);
     MPI_Errhandler_set(chameleon_comm_load, MPI_ERRORS_RETURN);
 
+#if PRINT_CONFIG_VALUES
     RELP("chameleon_init: VERSION %s\n", CHAMELEON_VERSION_STRING);
+#endif
 #ifdef CHAM_DEBUG
     mem_allocated = 0;
 #endif
 
     // load config values that were speicified by environment variables
     load_config_values();
-    #if PRINT_CONFIG_VALUES
+#if PRINT_CONFIG_VALUES
     print_config_values();
-    #endif
+#endif
 
     // initilize thread data here
     __rank_data.comm_rank = chameleon_comm_rank;

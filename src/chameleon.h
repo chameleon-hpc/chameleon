d2b89a64cd39abde115c84631cbad0ab3947d692
marvin.porsil <marvin.porsil@rwth-aachen.de>
2021-08-19 17:15:29 +0200

    Added compilation modes.

--- src/chameleon.h
+++ src/chameleon.h
@@ -217,10 +217,12 @@ TYPE_TASK_ID chameleon_get_task_id(cham_migratable_task_t *task);
 // ================================================================================
 // Functions and types related to task affinity
 // ================================================================================
+#if USE_TASK_AFFINITY
 struct task_aff_physical_data_location_t;
 task_aff_physical_data_location_t affinity_schedule(cham_migratable_task_t *task);
 void update_aff_addr_map(cham_migratable_task_t *task, int32_t new_gtid);
 task_aff_physical_data_location_t affinity_recalculate_task_data_loc(cham_migratable_task_t* task);
+#endif
 
 #ifdef __cplusplus
 }
